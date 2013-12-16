9f181c1c6ddfe1aa03cb06345718276ae68af5bd
John Hyland <jhyland@damballa.com>
2013-12-16 11:11:07 -0800

    add fish vi mode

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,9 +1,17 @@
+source ~/.config/fish/vi-mode.fish
 source ~/.config/fish/fish_prompt.fish
 source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
+#source /usr/local/share/chruby/auto.fish
+chruby 1.9.3
 
+set fish_greeting ''
 set PATH $PATH ~/bin
 set EDITOR vim
 
 alias vi "mvim"
 alias be "bundle exec"
+
+function fish_user_key_bindings
+  vi_mode_insert
+end
+
