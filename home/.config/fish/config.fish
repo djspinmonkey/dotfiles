3791f8644c9b29f65b81510490cee41b8b5c2736
John Hyland <jhyland@damballa.com>
2021-02-18 13:48:33 -0800

    Remove chruby from config.fish.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,8 +1,5 @@
 source ~/.config/fish/vi-mode.fish
 source ~/.config/fish/fish_prompt.fish
-source /usr/local/share/chruby/chruby.fish
-#source /usr/local/share/chruby/auto.fish
-chruby 1.9.3
 
 set fish_greeting ''
 set PATH $PATH ~/bin
